d7edbda6eb9e06882b73a58c128f210b203e4925
AyaulymSagat <47839407+AyaulymSagat@users.noreply.github.com>
2019-10-19 16:35:38 +0600

    Project
    Web project

---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetHelloWorldApplication.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MoviesContext>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                /*routes.MapRoute(
+                    name: "calculator",
+                    template: "Calculator/{action}/{number:int}",
+                    defaults: new { Controller = "Calculator" }
+                );*/
+
+                /*routes.MapRoute(
+                   name: "calculator",
+                   template: "{controller=Calculator}/addTenToNumber/{number:int}",
+                   defaults: new { action = "plusTen" }
+                );*/
+
+                /*routes.MapRoute(
+                    name: "messages",
+                    template: "say/{*message}",
+                    defaults: new {controller="Messages", action="ShowMessage"}
+                );*/
+
+                /*routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Hello}/{action=Index}/{id?}"
+                    );*/
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Movies}/{action=Index}/{id?}"
+                    );
+            });
+
+            app.UseStaticFiles();
+
+            // app.UseMvcWithDefaultRoute(); the same as previous line
+
+            //app.Run(async (context) =>
+            //{
+            //  await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
